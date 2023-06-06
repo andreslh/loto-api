4ae2a228fdfe058b30a312535f1dce19ac53f110
Andres Ledesma Higonet <aledesma@inventario.360.com>
2023-06-06 09:47:00 -0300

    feat(admin): rutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const clientsRouter = require('./clients');
 const usersRouter = require('./users');
-const { authenticateJWT } = require('../validators/authentication');
+const adminRouter = require('./admin');
+const { authenticateJWT, isAdmin } = require('../validators/authentication');
 
 const apiRouter = express.Router();
 
 apiRouter.use('/users', usersRouter);
+apiRouter.use('/admin', authenticateJWT, isAdmin, adminRouter);
 apiRouter.use('/clients', authenticateJWT, clientsRouter);
 
 module.exports = apiRouter;
