ab86fa338a424da0abb5eb32c669af017fdb0fb6
Andres Ledesma Higonet <aledesma@inventario.360.com>
2023-07-17 10:17:49 -0300

    fix get lottery

--- src/controllers/lottery.js
+++ src/controllers/lottery.js
@@ -3,7 +3,7 @@ const { getNumbersToTen, getNumbersToTwenty, getEmptyNumbersObj } = require('./h
 
 const get = async (req, res) => {
   try {
-    const lottery = await Lottery.findAll({ order: [['id', 'ASC']] });
+    const lottery = await Lottery.findAll({ order: [['date', 'DESC']], where: {current: false} });
     return res.status(200).json({ lottery });
   } catch (error) {
     return res.status(500).send(error.message);
